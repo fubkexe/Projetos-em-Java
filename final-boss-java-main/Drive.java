394749054a395d76ab9ed8496f3cba64affb5d9b
Luis Felipe Da Silva Curvello <165702358+fubkexe@users.noreply.github.com>
2025-11-06 20:57:24 -0300

    Atualizar o Drive.java

--- final-boss-java-main/Drive.java
+++ final-boss-java-main/Drive.java
@@ -1,9 +1,7 @@
 
 public class Drive {
-
-	public static void main(String[] args) {
-		
-
-	}
-
+    public static void main(String[] args) {
+        GameMap mapa = new GameMap();
+        mapa.sobreviver();
+    }
 }
