a845c1b7a6c0189e7d3929e8e2928f959676b31b
Luis Felipe Da Silva Curvello <165702358+fubkexe@users.noreply.github.com>
2025-11-06 20:58:02 -0300

    Atualizar o Beast.java

--- final-boss-java-main/Beast.java
+++ final-boss-java-main/Beast.java
@@ -1,56 +1,35 @@
 
-import java.util.Random;
-import java.util.Scanner;
+ import java.util.Random;
+
 public class Beast {
-	
-	
-	private String nome;
+    private String nome;
+    private int forca;
+    private int velocidade;
 
+    public Beast(String nome, int forca, int velocidade, int i) {
+        this.nome = nome;
+        this.forca = forca;
+        this.velocidade = velocidade;
+    }
 
-	private int forca;
+    // Lista de bestas disponíveis
+    static Beast[] lista = {
+        new Beast("Lobo", 60, 200, 0),
+        new Beast("Urso", 80, 150, 0),
+        new Beast("Veado", 30, 250, 0),
+        new Beast("Coelho", 10, 300, 0),
+        new Beast("Puma", 70, 220, 0),
+        new Beast("Tigre", 90, 180, 0),
+        new Beast("Cervo", 40, 240, 0),
+    };
 
+    public String getNome() {
+        return nome;
+    }
 
-	private int velocidade;
-
-
-	public Beast(String nome, int forca, int velocidade, int i) {
-		this.nome = nome;
-		this.forca = forca;
-		this.velocidade = velocidade;
-		
-	
-		
-		
-		
-	}
-
-
-	
-
-	static Beast[]lista = {
-			new Beast("Lobo", 60, 200, 0),
-			new Beast("Urso", 40, 310, 0),
-			new Beast("Veado", 60, 200, 0),
-			new Beast("Coelho", 40, 310, 0),
-			new Beast("Puma", 60, 200, 0),
-			new Beast("Tigre", 40, 310, 0),
-			new Beast("Cervo", 60, 200, 0),					
-	};
-	
-	
-
-
-	public int getRandomDamage(Random ran) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-
-	public String getNome() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	
-
+    public int getRandomDamage(Random ran) {
+        int variacao = ran.nextInt(20) - 10; // ±10 de variação
+        int dano = forca + variacao;
+        return Math.max(0, dano);
+    }
 }
